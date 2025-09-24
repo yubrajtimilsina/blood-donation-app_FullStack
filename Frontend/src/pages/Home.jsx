a5027216c69e29acd3f91f2cc395fe0e4726028c
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2025-09-24 09:46:48 +0545

    some feature added

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,14 +4,21 @@ import Hero from '../components/Hero'
 import Featured from '../components/Featured'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
+import{ Element } from "react-scroll";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Element name="hero">
       <Hero />
+      </Element>
+      <Element name="featured">
       <Featured />
+      </Element>
+      <Element name="contact">
       <Contact />
+      </Element>
       <Footer />
     </div>
   )
